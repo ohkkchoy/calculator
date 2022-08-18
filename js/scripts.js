a138b0f0c1e8a40a76128147469f8ee003f82075
ohkkchoy <110420268+ohkkchoy@users.noreply.github.com>
2022-08-17 19:52:22 -0700

    write an addition function and change strings to add properly.
    Co-authored-by: Manuel Raygoza  <manny.raygoza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
+//Business Logic:
 function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("The sum is " + add(3,5) + ".");
+
+//User Interface Logic:
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
