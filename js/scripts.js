68b7b16d022edd9465386596df18ae121f3efccb
ohkkchoy <110420268+ohkkchoy@users.noreply.github.com>
2022-08-17 19:26:42 -0700

    refactor call line of saysomething to be on one line.
    Co-authored-by: Manuel Raygoza  <manny.raygoza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,4 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-saySomething("hi");
-
-const result = add(3, 5);
-const outputText = "The sum is " + result + ".";
-saySomething(outputText);
+saySomething("The sum is " + add(3,5) + ".");
