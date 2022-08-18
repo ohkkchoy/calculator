ab18ad2f161971745afe55d5649f7a4d8e8e90b9
ohkkchoy <110420268+ohkkchoy@users.noreply.github.com>
2022-08-17 21:24:34 -0700

    window alert user with equation and answer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 //Business Logic:
 function add(number1, number2) {
   return number1 + number2;
+ 
 }
 
 function multiply(number1, number2) {
-  return number1 * number2
+  return number1 * number2;
 }
 
 function subtract(number1, number2) {
@@ -20,7 +21,7 @@ function division(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The addition of your numbers equals " + add(number1, number2));
-window.alert("The subtraction of your numbers equals " + subtract(number1, number2));
-window.alert("The multiplication of your numbers equals " + multiply(number1, number2));
-window.alert("The division of your numbers equals " + division(number1, number2));
+window.alert(number1 + " + " + number2 + " = " + add(number1, number2));
+window.alert(number1 + " - " + number2 + " = " + subtract(number1, number2));
+window.alert(number1 + " x " + number2 + " = " + multiply(number1, number2));
+window.alert(number1 + " / " + number2 + " = " + division(number1, number2));
